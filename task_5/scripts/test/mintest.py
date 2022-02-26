3deeba575a63003d5d38c9848e6e42c084c4a6dd
Gunjan Lunkad <67200542+9gl@users.noreply.github.com>
2022-02-26 23:07:24 +0530

    Update mintest.py

--- task_5/scripts/test/mintest.py
+++ task_5/scripts/test/mintest.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import rospy, time
-import cv2
+import rospy,cv2
 import cv2.aruco as aruco
 from geometry_msgs.msg import PoseStamped, Twist
 from mavros_msgs.msg import ParamValue, State
@@ -10,15 +9,17 @@ from std_msgs.msg import String, UInt8
 from gazebo_ros_link_attacher.srv import Gripper
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from multiprocessing import Process
 from threading import Thread
 import numpy as np
  
 class pick_n_place:
- 
+    
     def __init__(self):
         '''Initialize the service'''
         self.service = ""
+    
+    def __del__(self):
+        rospy.loginfo("Pick and Place Del")
 
     def setArm(self,drone_no):
         '''Calling to /mavros/cmd/arming to arm the drone'''
@@ -28,20 +29,20 @@ class pick_n_place:
             armService = rospy.ServiceProxy(self.service, CommandBool)
             armService(True)
         except rospy.ServiceException as e:
-            print("Service arming call failed for {0}: {1}".format(drone_no,e))
+            rospy.loginfo("Service arming call failed for {0}: {1}".format(drone_no,e))
  
     def offboard(self,drone_no):
-        '''Call /mavros/set_mode to set the mode the drone to OFFBOARD and print fail message on failure'''
+        '''Call /mavros/set_mode to set the mode the drone to OFFBOARD and rospy.loginfo fail message on failure'''
         self.service = '/'+drone_no+'/mavros/set_mode'
         rospy.wait_for_service(self.service)
         try:
             setMode = rospy.ServiceProxy(self.service, SetMode)
             setMode(base_mode = 0,custom_mode = "OFFBOARD")
         except rospy.ServiceException as e:
-            print("Offboard set mode failedfor {0}: {1}".format(drone_no,e))
+            rospy.loginfo("Offboard set mode failedfor {0}: {1}".format(drone_no,e))
  
  
-    def is_grab(self, drone_no,action):
+    def pick(self, drone_no,action):
         '''Call /activate_gripper to pick the box'''
         self.service = drone_no+'/activate_gripper'
         rospy.wait_for_service(self.service)
@@ -49,9 +50,8 @@ class pick_n_place:
             picked = rospy.ServiceProxy(self.service, Gripper)
             picked(action)
         except rospy.ServiceException as e:
-            print("activate_gripper Service failed for {0}: {1}".format(drone_no,e))
+            rospy.loginfo("activate_gripper Service failed for {0}: {1}".format(drone_no,e))
  
-
     def land(self, drone_no):
         '''Call /mavros/cmd/land to land'''
         self.service = drone_no+'/mavros/cmd/land'
@@ -60,7 +60,7 @@ class pick_n_place:
             land_mode = rospy.ServiceProxy(self.service, CommandTOL)
             land_mode(min_pitch=0, yaw=0, latitude=0 ,longitude=0, altitude=0)
         except rospy.ServiceException as e:
-            print ("Land mode failed: {0}".format(e))
+            rospy.loginfo ("Land mode failed: {0}".format(e))
 
  
 class stateMoniter:
@@ -90,7 +90,7 @@ class stateMoniter:
         try:
             self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8") # Converting the image to OpenCV standard image
         except CvBridgeError as e:
-            print(e)
+            rospy.loginfo(e)
 
 class Aruco:
  
@@ -100,7 +100,7 @@ class Aruco:
         aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_250)
         parameters = aruco.DetectorParameters_create()
         corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters = parameters)
-        Detected_ArUco_markers = dict(zip(ids[:,0], corners)) if ids != None else 0     
+        Detected_ArUco_markers = dict(zip(ids[:,0], corners)) and ids 
         return Detected_ArUco_markers
  
     def Calculate_orientation(self,Detected_ArUco_markers):
@@ -113,6 +113,9 @@ class Aruco:
 
 
 class strawberry_stacker:
+    
+    def grid_location(self):
+        cells = []
 
     def box_count(self):
         pass
@@ -144,6 +147,9 @@ class Drone(object):
         self.found_count = 0
         self.prev_loc = (0,0,0)
         self.row_nos_lst = list()
+    
+    def __del__(self):
+        rospy.loginfo("Drone D!")
 
     def setup(self):
         rospy.Subscriber(self.drone_no+'/mavros/state', State, self.st_mt.stateCb)
@@ -156,12 +162,12 @@ class Drone(object):
         while not self.st_mt.state.armed:
             self.pp.setArm(self.drone_no)
             self.rate.sleep()
-        print("Armed!! {0}".format(self.drone_no))
+        rospy.loginfo("Armed!! {0}".format(self.drone_no))
         while not self.st_mt.state.mode == "OFFBOARD":
-            print(self.st_mt.state.mode)
+            rospy.loginfo(self.st_mt.state.mode)
             self.pp.offboard(self.drone_no)
             self.rate.sleep()
-        print("OFFBOARD mode activated {0}".format(self.drone_no))
+        rospy.loginfo("OFFBOARD mode activated {0}".format(self.drone_no))
         
     def reach_point(self,px,py,pz):
         reached = False
@@ -170,36 +176,37 @@ class Drone(object):
                 reached = True
             self.rate.sleep()
         if self.debug:
-            print("Reached : ",(px,py,pz))
+            rospy.loginfo("Reached : ",(px,py,pz))
 
     def pick_from_location(self):
         self.prev_loc = (self.st_mt.pos.pose.position.x,self.st_mt.pos.pose.position.y,3.0)
-        while self.found:
-            print(self.st_mt.grip.data)
+        pick = False
+        while not pick:
+            rospy.loginfo(self.st_mt.grip.data)
             if self.st_mt.grip.data == "True":
                 self.found_count += 1 
-                self.pp.is_grab(self.drone_no, True)
+                self.pp.pick(self.drone_no, True)
                 self.found = 0
-                print("Grabbed!!!!!")
+                rospy.loginfo("Grabbed!!!!!")
                 self.vel.linear.x,self.vel.linear.y,self.vel.linear.z = (10,10,3)
                 self.local_vel_pub.publish(self.vel)
                 self.drop[-1] = self.prev_loc
-            else:
-                self.rate.sleep()
-        
+                pick = True
+            self.rate.sleep()
+
     def drop_at_location(self):
         for i,setpoint in enumerate(self.drop):
-            print(setpoint)
+            rospy.loginfo(setpoint)
             self.vel.linear.x,self.vel.linear.y,self.vel.linear.z = (10,10,1)
             self.local_vel_pub.publish(self.vel)
             self.epos.pose.position.x,self.epos.pose.position.y,self.epos.pose.position.z = setpoint
             self.local_pos_pub.publish(self.epos)    
             self.reach_point(setpoint[0],setpoint[1],setpoint[2])
             if self.st_mt.pos.pose.position.z < 4:
-                self.pp.is_grab(self.drone_no,False)
-            print("Done with one box")
+                self.pp.pick(self.drone_no,False)
+            rospy.loginfo("Done with one box")
             self.found = 0
-            print("Found_count: {0}".format(self.found_count))
+            rospy.loginfo("Found_count by {0}: {1} ".format(self.drone_no,self.found_count))
         # self.drop.append(self.lpos)
 
     def search(self):
@@ -213,76 +220,76 @@ class Drone(object):
                 else:
                     self.drop = self.drop_loc[1]
                 y1,z1 = self.st_mt.pos.pose.position.y, self.st_mt.pos.pose.position.z
-                print("Aruco Check")
+                rospy.loginfo("Aruco Check")
                 while not self.found:
-                    # print("in self.found")
+                    # rospy.loginfo("in self.found")
                     try:    
-                        # print("Try !")
+                        # rospy.loginfo("Try !")
                         cx,cy = self.aruco.Calculate_orientation(self.aruco.detect_ArUco(self.st_mt.img))
-                        print(cx,cy ,": center")
+                        rospy.loginfo(cx,cy ,": center")
                         if(abs(cx - 200 )<= 3  and abs(cy - 200 )<= 3 ) :
-                            print("Ready to pick up ")
-                            z1 = self.epos.pose.position.z = -0.1
+                            rospy.loginfo("Ready to pick up ")
+                            z1 = self.epos.pose.position.z = 0.13
                             y1= self.epos.pose.position.y = self.st_mt.pos.pose.position.y + 0.24
                             self.local_pos_pub.publish(self.epos)
                             self.found = 1
                             while not reached:
-                                if abs(y1 - self.st_mt.  pos.pose.position.y) < 0.2 and abs(z1 - self.st_mt.pos.pose.position.z) < 0.25 :
+                                if abs(self.epos.pose.position.x - self.st_mt.pos.pose.position.x) < 0.15 and abs(y1 - self.st_mt.pos.pose.position.y) < 0.2 and abs(z1 - self.st_mt.pos.pose.position.z) < 0.25 :
                                     reached = True
                                 self.rate.sleep()
-                            print("landing coordinates",x1,y1,z1)
+                            rospy.loginfo("landing coordinates",x1,y1,z1)
                         elif(cx - 200 < -3.1 and abs(cx -200 ) <= 15) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x - 0.15
-                            print("decrese x 0.1 ")
+                            rospy.loginfo("decrese x 0.1 ")
                         elif(cx - 200 < -15.1 and abs(cx -200 ) <= 40) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x - 0.2
-                            print("decrese x 0.2 ")
+                            rospy.loginfo("decrese x 0.2 ")
                         elif(cx - 200 < -40.1 and abs(cx -200 ) <= 85) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x - 0.5
-                            print("decrese x 0.5 ")
+                            rospy.loginfo("decrese x 0.5 ")
                         elif(cx - 200 < -85.1 ) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x - 1
-                            print("decrese x 1 ")
+                            rospy.loginfo("decrese x 1 ")
                         
                         elif(cx - 200 > 3 and abs(cx-200) <= 15) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x + 0.15
-                            print("increase ++ x 0.1")
+                            rospy.loginfo("increase ++ x 0.1")
                         elif(cx - 200 > 15.1 and abs(cx-200) <= 40) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x + 0.2
-                            print("increase ++ x 0.2")
+                            rospy.loginfo("increase ++ x 0.2")
                         elif(cx - 200 > 40.1 and abs(cx-200) <= 85) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x + 0.5
-                            print("increase ++ x 0.5")
+                            rospy.loginfo("increase ++ x 0.5")
                         elif(cx - 200 > 85) :
                             x1= self.epos.pose.position.x  = self.st_mt.pos.pose.position.x + 1
-                            print("increase ++ x 1")
+                            rospy.loginfo("increase ++ x 1")
                         
                         # ------------------------------------------------------y coordinate -------------------------------------------------------------
                         elif(cy - 200 < -3.1 and abs(cy -200 ) <= 15) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y + 0.15
-                            print("decrese y 0.1 ")
+                            rospy.loginfo("decrese y 0.1 ")
                         elif(cy - 200 < -15.1 and abs(cy -200 ) <= 40) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y + 0.2
-                            print("decrese y 0.2 ")
+                            rospy.loginfo("decrese y 0.2 ")
                         elif(cy - 200 < -40.1 and abs(cy -200 ) <= 85) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y + 0.5
-                            print("decrese y 0.5 ")
+                            rospy.loginfo("decrese y 0.5 ")
                         elif(cy - 200 < -85.1 ) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y + 1
-                            print("decrese y 1 ")
+                            rospy.loginfo("decrese y 1 ")
                         
                         elif(cy - 200 > 3 and abs(cy-200) <= 15) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y - 0.15
-                            print("increase ++ y 0.1")
+                            rospy.loginfo("increase ++ y 0.1")
                         elif(cy - 200 > 15.1 and abs(cy-200) <= 40) :
                             y1= self.epos.pose.position.y = self.st_mt.pos.pose.position.y - 0.2
-                            print("increase ++ x 0.2")
+                            rospy.loginfo("increase ++ x 0.2")
                         elif(cy - 200 > 40.1 and abs(cy-200) <= 85) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y - 0.5
-                            print("increase ++ y 0.5")
+                            rospy.loginfo("increase ++ y 0.5")
                         elif(cy - 200 > 85) :
                             y1= self.epos.pose.position.y  = self.st_mt.pos.pose.position.y - 1
-                            print("increase ++ y 1")
+                            rospy.loginfo("increase ++ y 1")
                     except:
                         continue
                     self.local_pos_pub.publish(self.epos)
@@ -295,15 +302,15 @@ class Drone(object):
         self.setup()
         if self.drone_no == "edrone0":
             for row_no in row_nos_lst:
-                print(self.row_nos_lst)
+                rospy.loginfo(self.row_nos_lst)
                 row_nos_lst.pop(self.found_count)
-                print("row list {0} : {1}".format(self.row_nos_lst,self.drone_no))
+                rospy.loginfo("row list {0} : {1}".format(self.row_nos_lst,self.drone_no))
                 if row_no % 2 == 0 and row_no != 0:
                     x ,y ,z =  0.5 ,row_no * 4 , self.alt 
                     self.epos.pose.position.x,self.epos.pose.position.y,self.epos.pose.position.z = (x,y,z)
                     self.local_pos_pub.publish(self.epos)
                     self.reach_point(self.epos.pose.position.x,self.epos.pose.position.y,self.epos.pose.position.z)
-                    print(x,y,z)
+                    rospy.loginfo(x,y,z)
                     self.find = 1
                     self.search()
                     self.pick_from_location()
@@ -311,13 +318,14 @@ class Drone(object):
         elif self.drone_no == "edrone1":
             for row_no in row_nos_lst:
                 row_nos_lst.pop(self.found_count)
-                print("row list {0} : {1}".format(self.row_nos_lst,self.drone_no))
+                rospy.loginfo("row list {0} : {1}".format(self.row_nos_lst,self.drone_no))
+                rospy.loginfo(self.drone_no,"+",row_no)
                 if row_no % 2 != 0 and row_no != 0:
-                    x ,y ,z = 1 ,row_no * 4  - 60.0, self.alt+1
+                    x ,y ,z = 1 ,row_no * 4  - 60.0, self.alt+0.75
                     self.epos.pose.position.x,self.epos.pose.position.y,self.epos.pose.position.z = (x,y,z)
                     self.local_pos_pub.publish(self.epos)
                     self.reach_point(self.epos.pose.position.x,self.epos.pose.position.y,self.epos.pose.position.z)
-                    print(x,y,z)
+                    rospy.loginfo(x,y,z)
                     self.find = 1
                     self.search()
                     self.pick_from_location()
@@ -341,10 +349,9 @@ def main():
     drop_loc2 = ([(16.31,-66.55,4.25),(16.31,-66.55,2.25),(16.31,-66.55,5.25),(0,0,5.25)],[(58.5,3.74,4.25),(58.5,3.75,2.25),(58.5,3.75,5.25), (0,0,5.25)])
     d1,d2 = Drone(e1,drop_loc1), Drone(e2,drop_loc2)
     t1 = Thread(target = d1.drone)
-    t1.start()    
+    t1.start() 
     t2 = Thread(target = d2.drone)
     t2.start()
-    t2.join()
 
 if __name__ == '__main__':
     print(__file__.split("/")[-1])
@@ -352,5 +359,4 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-
 
